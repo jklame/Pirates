5aca0cecd7d38cd9109b57048279742cbd12d072
jklame <jklame@users.noreply.github.com>
2015-05-28 08:11:00 -0400

    checkstyle

--- src/main/java/com/jklame/pirates/lib/PermutationSet.java
+++ src/main/java/com/jklame/pirates/lib/PermutationSet.java
@@ -6,28 +6,28 @@ import java.util.List;
 
 public class PermutationSet<T> implements Iterable<List<T>>
 {
-	private List<T> masterList;
+    private final List<T> masterList;
 
-	public PermutationSet(final List<T> masterList)
-	{
-		super();
-		this.masterList = masterList;
-	}
+    public PermutationSet(final List<T> masterList)
+    {
+        super();
+        this.masterList = masterList;
+    }
 
-	@Override
-	public Iterator<List<T>> iterator()
-	{
-		return new PermutationIterator<T>(masterList);
-	}
-	
-	public static void main(final String[] args)
-	{
-		final List<String> list = Arrays.asList("a", "a", "a", "a", "b", "a", "c");
-		final PermutationSet<String> set = new PermutationSet<String>(list);
-		for (final List<String> permutations : set)
-		{
-			System.out.println(permutations);
-		}
-		
-	}
+    @Override
+    public Iterator<List<T>> iterator()
+    {
+        return new PermutationIterator<T>(masterList);
+    }
+    
+    public static void main(final String[] args)
+    {
+        final List<String> list = Arrays.asList("a", "a", "a", "a", "b", "a", "c");
+        final PermutationSet<String> set = new PermutationSet<String>(list);
+        for (final List<String> permutations : set)
+        {
+            System.out.println(permutations);
+        }
+        
+    }
 }
